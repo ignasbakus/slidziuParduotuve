c482b04cb31a35ec80837fc366c67b105a96ffc3
LeBrounius <ignas.bakus77@gmail.com>
2023-12-16 17:19:54 +0200

    Create mygtuko nuoroda i create.php puslapi

---
+++ views/categories/create.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="createInputs">
+            <label for="id">ID:</label>
+            <input type="text" class="createCategoryInputs" id="id" placeholder="Enter category's ID">
+        </div>
+        <div class="createInputs">
+            <label for="name">Name:</label>
+            <input type="text" class="createCategoryInputs" id="name" placeholder="Enter category's name">
+        </div>
+        <div class="createInputs">
+            <label for="description">Description:</label>
+            <input type="text" class="createCategoryInputs" id="description" placeholder="Enter category's description">
+        </div>
+        <div class="createInputs">
+            <label for="photo">Photo:</label>
+            <input type="text" class="createCategoryInputs" id="photo" placeholder="Enter photo url">
+        </div>
+    </div>
+</body>
+
+</html>
--- views/categories/index.php
+++ views/categories/index.php
@@ -13,6 +13,9 @@ $categories = CategoryController::getAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
@@ -29,7 +32,7 @@ $categories = CategoryController::getAll();
         <div class="row">
             <div class="col-1"></div>
             <div class="col-10">
-                <form class="buttons" action="" method="post">
+                <form class="buttons" action="./create.php" method="post">
                     <button class="buttonCreate btnAll" type="submit">Create</button>
                 </form>
             </div>
@@ -39,7 +42,7 @@ $categories = CategoryController::getAll();
             <div class="col-1"></div>
             <div class="col-10">
                 <?php foreach ($categories as $key => $category) { ?>
-                    <div class="row">
+                    <div class="row mainBodyRow">
                         <div class="col-6 nameAndDescription">
                             <h3 class='skiTypeName'><?= $category->name ?></h3>
                             <p class='skiTypeDescription'><?= $category->description ?></p>
@@ -47,7 +50,7 @@ $categories = CategoryController::getAll();
                         <div class="col-4 ">
                             <img class="skiImages" src="<?= $category->photo ?>" alt="">
                         </div>
-                        <div class="col-2 buttons">
+                        <div class="col-2 buttons borderButtonsCol">
                             <form action="" method="post">
                                 <button class="btn1 btnAll" type="submit">Show</button>
                             </form>
