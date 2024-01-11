4b67dbc5d5676141e29c94230703ba9bf834c9db
LeBrounius <ignas.bakus77@gmail.com>
2024-01-11 18:34:42 +0200

    sutvarkyti catgeories edit.php ir create.php failai css aspektu

--- views/categories/create.php
+++ views/categories/create.php
@@ -7,6 +7,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header("location: ./index.php");
 }
 
+$categories = CategoryController::getAll();
+
+include_once "../components/header.php";
+
 ?>
 
 <!DOCTYPE html>
@@ -15,38 +19,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/reset.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/main.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-            <form action="./create.php" method="post">
-                <div class="createInputs">
-                    <label for="name">Kategorijos pavadinimas: </label>
-                    <input type="text" class="createCategoryInputs" name="name" placeholder="Įveskite kategorijos pavadinimą">
-                </div>
-                <div class="createInputs">
-                    <label for="description">Kategorijos aprašymas: </label>
-                    <input type="text" class="createCategoryInputs" name="description" placeholder="Įveskite kategorijos aprašymą">
-                </div>
-                <div class="createInputs">
-                    <label for="photo">Nuotraukos URL: </label>
-                    <input type="text" class="createCategoryInputs" name="photo" placeholder="Įveskite nuotraukos URL">
-                </div>
-                <button type="submit" name="">Pateikti</button>
-            </form>
+    <div class="categoryCreateContainer">
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6 backgroundCreateAndEdit">
+                <form action="./create.php" method="post">
+                    <h2 class="headingCreateAndEdit" >Sukurti naują kategoriją</h2>
+                    <div class="createInputs">
+                        <label class="labelCreateAndEdit" for="name">Kategorijos pavadinimas: </label><br>
+                        <input type="text" class="createCategoryInputs" name="name" placeholder="Įveskite kategorijos pavadinimą">
+                    </div>
+                    <div class="createInputs">
+                        <label class="labelCreateAndEdit" for="description">Kategorijos aprašymas: </label><br>
+                        <input type="text" class="createCategoryInputs" name="description" placeholder="Įveskite kategorijos aprašymą">
+                    </div>
+                    <div class="createInputs">
+                        <label class="labelCreateAndEdit" for="photo">Nuotraukos URL: </label><br>
+                        <input type="text" class="createCategoryInputs" name="photo" placeholder="Įveskite nuotraukos URL">
+                    </div>
+                    <button class="createAndEditSubmitButton" type="submit" name="">Pateikti</button>
+                </form>
+            </div>
+            <div class="col-3"></div>
         </div>
-        <div class="col-3"></div>
     </div>
-
-
-
-
-
-
 </body>
 
 </html>
--- views/categories/edit.php
+++ views/categories/edit.php
@@ -2,7 +2,7 @@
 
 include "../../controllers/CategoryController.php";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     CategoryController::update($_POST['id']);
     header("location: ./index.php");
 }
@@ -12,7 +12,9 @@ if (!isset($_GET["id"])) {
 }
 
 $category = CategoryController::find($_GET["id"]);
-// print_r($category);die;
+$categories = CategoryController::getAll();
+
+include_once "../components/header.php";
 ?>
 
 <!DOCTYPE html>
@@ -21,28 +23,41 @@ $category = CategoryController::find($_GET["id"]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/reset.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/main.css">
     <title>Document</title>
 </head>
+
 <body>
-    <form action="./edit.php" method="post">
-        <div class="container">
-            <div class="createInputs">
-                <label for="name">Name:</label>
-                <input type="text" class="createCategoryInputs" name="name" placeholder="Enter category's name" value="<?= $category->name ?>">
+    <div class="categoryEditContainer">
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6 backgroundCreateAndEdit">
+                <form action="./edit.php" method="post">
+                    <h2 class="headingCreateAndEdit">Pataisyti kategoriją</h2>
+                    <div class="editInputs">
+                        <label class="labelCreateAndEdit" for="name">Kategorijos pavadinimas: </label><br>
+                        <input type="text" class="createCategoryInputs" name="name" placeholder="Įveskite kategorijos pavadinimą" value="<?= $category->name ?>">
+                    </div>
+                    <div class="editInputs">
+                        <label class="labelCreateAndEdit" for="description">Description:</label><br>
+                        <input type="text" class="createCategoryInputs" name="description" placeholder="Įveskite kategorijos aprašymą" value="<?= $category->description ?>">
+                    </div>
+                    <div class="editInputs">
+                        <label class="labelCreateAndEdit" for="photo">Photo:</label><br>
+                        <input type="text" class="createCategoryInputs" name="photo" placeholder="Įveskite nuotraukos URL" value="<?= $category->photo ?>">
+                    </div>
+                    <input type="hidden" name="id" value="<?= $category->id ?>">
+                    <button type="submit" class="createAndEditSubmitButton">Pateikti</button>
+                </form>
             </div>
-            <div class="createInputs">
-                <label for="description">Description:</label>
-                <input type="text" class="createCategoryInputs" name="description" placeholder="Enter category's description" value="<?= $category->description ?>">
-            </div>
-            <div class="createInputs">
-                <label for="photo">Photo:</label>
-                <input type="text" class="createCategoryInputs" name="photo" placeholder="Enter photo url" value="<?= $category->photo ?>">
-            </div>
-            <input type="hidden" name="id" value="<?= $category->id ?>">
-            <button type="submit" class="btnAll">Submit</button>
+            <div class="col-3"></div>
         </div>
-    </form>
+    </div>
 </body>
 
 </html>
